b24b907af53d1612aacc0ead35ee766cf9aabb06
kainhuck <kainhuck@163.com>
2020-02-03 14:22:27 +0800

    更新函数说明

--- sql_func.py
+++ sql_func.py
@@ -2,9 +2,14 @@
 def insert(table_name, **kwargs):
     '''
     构造sql插入语句
-    :param table_name:
-    :param kwargs:
-    :return:
+
+    :param table_name: 要插入的表名  type: string
+    :param kwargs: 要插入的属性名和属性值
+    :return: sql语句
+
+    INSERT INTO table_name ( field1, field2,...fieldN )
+                       VALUES
+                       ( value1, value2,...valueN );
     '''
     # 创建sql语句
     field = '('
@@ -23,7 +28,12 @@ def insert(table_name, **kwargs):
 def delete(table_name, **kwargs):
     """
     构造sql删除语句
-    delete from <table_name>[ where <>="<>" and <>="<>"];
+
+    :param table_name: 要删除的表名  type: string
+    :param kwargs: 指定属性名和属性值(约束)
+    :return: sql语句
+
+    DELETE FROM table_name [WHERE Clause]
     """
     field = ''
     if len(kwargs):
@@ -35,8 +45,15 @@ def delete(table_name, **kwargs):
     return sql
 
 
-def update(table_name, new_items, where={}):
+def update(table_name, new_items={}, where={}):
     """
+    构造sql更新语句
+
+    :param table_name: 要更新的表名  type: string
+    :param kwargs: 要插入的属性名和属性值  type: dict
+    :param where: 约束  type: dict
+    :return: sql语句
+
     UPDATE table_name SET field1=new-value1, field2=new-value2
     [WHERE Clause]
     """
@@ -59,10 +76,13 @@ def update(table_name, new_items, where={}):
 
 def select(table_name, column_names=[], where={}, limit=None, offset=0):
     """
-    column_names : 待查询的属性
-    where : 约束
-    limit : 返回记录数
-    offset : SELECT语句开始查询的数据偏移量
+    构造sql选择语句
+
+    :param column_names: 待查询的属性  type: list
+    :param where : 约束  type: dict
+    :param limit : 返回记录数  type: int
+    :param offset : SELECT语句开始查询的数据偏移量  type: int
+    :return: sql语句
 
     SELECT column_name,column_name
     FROM table_name

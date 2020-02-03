b03377dc90d20820ac2fffb8b4a96a1c3fef7e9c
kainhuck <kainhuck@163.com>
2020-02-03 13:08:32 +0800

    ..

--- demo.py
+++ demo.py
@@ -13,15 +13,13 @@ mysql = {
 if __name__ == '__main__':
 
     # # 使用方法
-    with Mysql(**mysql) as m:
-        m.executeSql(select("imgs",
-               column_names=["src", "alert"],
-               where={
-                   "id": 1234,
-                   "alert": "test"
-               },
-               limit=100,
-               offset=10))
+    # with Mysql(**mysql) as m:
+    #     results = m.select("imgs",
+    #            column_names=["src", "alert"],
+    #            limit=100,
+    #            offset=10)
+    #     for each in results:
+    #         print(each)
     # print(insert("imgs", src="https://demo.com/test.jpg", alert="test"))
     # print(delete("imgs", id=12, alert="test"))
     # print(
@@ -33,4 +31,6 @@ if __name__ == '__main__':
     #            },
     #            limit=100,
     #            offset=10))
-
+    with Mysql(**mysql) as m:
+        # m.insert("imgs", src="https://example.com/test.jpg", alert="test")
+        m.delete("imgs", id=353)
